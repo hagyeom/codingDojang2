2f48d155786be94ad49aa33a29826631dd654d86
하겸 윤 <younha3710@naver.com>
2023-07-07 19:51:35 +0900

    commit

---
+++ codingDojang2/enum_for.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef enum _DayOfWeek {
+	Sunday=0,
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	DayOfWeekCount
+} DayOfWeek;
+
+int main() {
+	for (DayOfWeek i = Sunday; i < DayOfWeekCount; i++) {
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
