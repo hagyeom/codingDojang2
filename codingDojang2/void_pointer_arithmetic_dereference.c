0c278af0e2c6a08493ff1d38c34c4d9e7014a2e5
하겸 윤 <younha3710@naver.com>
2023-07-08 20:50:48 +0900

    commit

---
+++ codingDojang2/void_pointer_arithmetic_dereference.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int numArr[5] = { 11,22,33,44,55 };
+	void* ptr = &numArr[2];
+
+	printf("%d\n", *(int*)ptr);
+
+	printf("%d\n", *((int*)ptr + 1));
+	printf("%d\n", *((int*)ptr - 1));
+
+	printf("%d\n", *(++(int*)ptr));
+	printf("%d\n", *(--(int*)ptr));
+
+	printf("%d\n", *(((int*)ptr)++));
+	printf("%d\n", *(((int*)ptr)--));
+
+	return 0;
+}
